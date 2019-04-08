5ccb37a872ab8b95dbf8cde1629073238ad3f28a
Anton Ostanin <anton.ostanin17@gmail.com>
2019-04-08 17:47:13 +0300

    Добавьте файлы проекта.

---
+++ RevitLogProjectLocation/PlanDimensionsApplication.cs
@@ -0,0 +1,51 @@
+﻿namespace RevitLogProjectLocation
+{
+    using System;
+    using System.Diagnostics;
+    using Autodesk.Revit.UI;
+    using Autodesk.Revit.UI.Events;
+
+    /// <summary>
+    /// Application
+    /// </summary>
+    public class LogApplication : IExternalApplication
+    {
+        /// <summary>
+        /// Событие загрузки приложения
+        /// </summary>
+        /// <param name="application">Апп</param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                application.DialogBoxShowing +=
+                    new EventHandler<DialogBoxShowingEventArgs>(AppDialogShowing);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Событие закрытия приложения
+        /// </summary>
+        /// <param name="application">Апп</param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        private void AppDialogShowing(object sender, DialogBoxShowingEventArgs e)
+        {
+            if (!(e is TaskDialogShowingEventArgs window)) return;
+            var type = window.DialogId;
+            var msg = window.Message;
+        }
+    }
+}

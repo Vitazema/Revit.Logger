46b3ac7c2afd04e526dc703a16e525b85d912cf5
Anton Ostanin <anton.ostanin17@gmail.com>
2019-04-09 15:36:04 +0300

    Решили пункт 2 тз

--- RevitLogProjectLocation/FailureProcessor.cs
+++ RevitLogProjectLocation/FailureProcessor.cs
@@ -7,9 +7,9 @@ namespace RevitLogProjectLocation
     using Revit_Lib;
 
     /// <summary>
-    /// Обработчик сообщений Ревит
+    /// Обработчик ошибок Ревит
     /// </summary>
-    public static class FailureProcessor
+    public class FailureProcessor : IFailuresProcessor
     {
         /// <summary>
         /// Обработка внутренних сообщений Revit
@@ -20,17 +20,67 @@ namespace RevitLogProjectLocation
         {
             try
             {
+                /*FailuresAccessor failuresAccessor = e.GetFailuresAccessor();
+                string transactionName = failuresAccessor.GetTransactionName();
+                failuresAccessor.DeleteAllWarnings();
+                IList<FailureMessageAccessor> fmas = failuresAccessor.GetFailureMessages();
+                if (fmas.Count == 0)
+                {
+                    e.SetProcessingResult(FailureProcessingResult.Continue);
+                    return;
+                    Общие площадки в связи «link.rvt» были изменены, но не сохранены.
+                    При повторном открытии экземпляры связи будут сброшены до последнего сохранения.
+                    Вы можете сохранить связь позже в диалоговом окне «Диспетчер связей».
+                  GetFailureDefinitionId =  cb11f811-2e36-4ab7-bce4-92eee381f058
+                }
+
+                foreach (FailureMessageAccessor fma in fmas)
+                {
+                    try
+                    {
+                        failuresAccessor.ResolveFailure(fma);
+                    }
+                    catch
+                    {
+                    }
+                }*/
+
                 var f = e.GetFailuresAccessor();
                 var transName = f.GetTransactionName();
-                if (!transName.Equals("Изменение исходных общих координат"))
+                if (!transName.Equals("Изменение исходных общих координат")
+                    && !transName.Equals("Перетаскивание")
+                    && !transName.Equals("Перенести"))
                     return;
+
+                var failures = f.GetFailureMessages();
+                if (failures.Count == 0)
+                {
+                    e.SetProcessingResult(FailureProcessingResult.Continue);
+                    return;
+                }
+
                 if (!AccessHelper.IsBIM)
-                    e.SetProcessingResult(FailureProcessingResult.ProceedWithRollBack);
+                {
+                    foreach (FailureMessageAccessor fm in failures)
+                    {
+                        var description = fm.GetDescriptionText();
+                        e.SetProcessingResult(FailureProcessingResult.WaitForUserInput);
+                    }
+                }
             }
             catch
             {
-                // ignored
             }
+        }
+
+        public FailureProcessingResult ProcessFailures(FailuresAccessor data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dismiss(Document document)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
